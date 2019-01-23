4c8f80f0617174aa9cb429b3fc3a763f3af22e50
jeetiss <za5rubley@gmail.com>
2019-01-23 16:09:11 +0300

    add progress

--- bin/index.js
+++ bin/index.js
@@ -3,7 +3,19 @@ const fs = require("fs");
 const { promisify } = require("util");
 const readline = require("readline");
 const Queue = require("p-queue");
+const cliSpinners = require("cli-spinners");
+const ora = require("ora");
 const verify = promisify(verifier.verify);
+
+const pickRandomValue = obj => {
+  const keys = Object.keys(obj);
+
+  const randomValue = Math.round(Math.random() * (keys.length - 1));
+
+  return obj[keys[randomValue]];
+};
+
+const spinner = ora({ text: "wtf...", spinner: pickRandomValue(cliSpinners) });
 
 const queue = new Queue({ concurrency: 10 });
 
@@ -11,14 +23,29 @@ const tool = readline.createInterface({
   input: fs.createReadStream("info.txt")
 });
 
-const output = fs.createWriteStream("results.txt");
+const outputFile = 'results.txt'
+const output = fs.createWriteStream(outputFile);
+
+let firstLine = true;
 
 tool.on("line", line => {
-  queue
-    .add(() => verify(line))
-    .then(info => info.success)
-    .catch(() => false)
-    .then(result => {
-      output.write(`${line}, ${+result}\n`);
+  spinner.start();
+
+  queue.add(() =>
+    verify(line)
+      .then(info => info.success)
+      .catch(() => false)
+      .then(result => {
+        spinner.text = `${line} is ${result ? "valid" : "invalid"}`;
+        output.write(`${line}, ${+result}\n`);
+      })
+  );
+
+  if (firstLine) {
+    firstLine = false;
+
+    queue.onIdle().then(() => {
+      spinner.stopAndPersist({ symbol: "✅", text: `results in ${outputFile}` });
     });
+  }
 });
