a35eac9cddd4f70e919fd37f33b8b344a82f426b
jeetiss <za5rubley@gmail.com>
2019-01-23 17:13:59 +0300

    arv

--- bin/index.js
+++ bin/index.js
@@ -5,6 +5,8 @@ const readline = require("readline");
 const Queue = require("p-queue");
 const cliSpinners = require("cli-spinners");
 const ora = require("ora");
+const arg = require("arg");
+
 const verify = promisify(verifier.verify);
 
 const pickRandomValue = obj => {
@@ -16,36 +18,58 @@ const pickRandomValue = obj => {
 };
 
 const spinner = ora({ text: "wtf...", spinner: pickRandomValue(cliSpinners) });
-
 const queue = new Queue({ concurrency: 10 });
-
-const tool = readline.createInterface({
-  input: fs.createReadStream("info.txt")
-});
-
-const outputFile = 'results.txt'
-const output = fs.createWriteStream(outputFile);
-
 let firstLine = true;
 
-tool.on("line", line => {
-  spinner.start();
+try {
+  const args = arg({
+    // Types
+    "--help": Boolean,
+    "--output": String,
 
-  queue.add(() =>
-    verify(line)
-      .then(info => info.success)
-      .catch(() => false)
-      .then(result => {
-        spinner.text = `${line} is ${result ? "valid" : "invalid"}`;
-        output.write(`${line}, ${+result}\n`);
-      })
-  );
+    // Aliases
+    "-h": "--help",
+    "-o": "--output"
+  });
 
-  if (firstLine) {
-    firstLine = false;
+  console.log(args);
 
-    queue.onIdle().then(() => {
-      spinner.stopAndPersist({ symbol: "✅", text: `results in ${outputFile}` });
-    });
+  const inputFile = args["_"][0];
+  const outputFile = args["--output"] || "result.txt";
+
+  if (!inputFile) {
+    throw Error("Requires default argument for input file: email-verificator [FILE]");
   }
-});
+
+  const output = fs.createWriteStream(outputFile);
+  const tool = readline.createInterface({
+    input: fs.createReadStream("info.txt")
+  });
+
+  tool.on("line", line => {
+    spinner.start();
+
+    queue.add(() =>
+      verify(line)
+        .then(info => info.success)
+        .catch(() => false)
+        .then(result => {
+          spinner.text = `${line} is ${result ? "valid" : "invalid"}`;
+          output.write(`${line}, ${+result}\n`);
+        })
+    );
+
+    if (firstLine) {
+      firstLine = false;
+
+      queue.onIdle().then(() => {
+        spinner.stopAndPersist({
+          symbol: "✅",
+          text: `results in ${outputFile}`
+        });
+      });
+    }
+  });
+} catch (err) {
+  console.log(err.message);
+}
